3e6ede19c94eba5a033a3ca53389b6b00a3b2bc6
Pedro Fiuza <pfiuza15@gmail.com>
2023-11-12 19:39:27 -0300

    função 1/2

--- lab.c
+++ lab.c
@@ -2,13 +2,17 @@
 #include <stdlib.h>
 #include "lab.h"
 
+struct Task tasks[100];
+int numTasks = 0;
+
 void cadastrarTarefa(struct Task tasks[], int *numTasks) {
+    struct Task newTask;
+    
     if (*numTasks >= 100) {
         printf("A lista de tarefas está cheia. Não é possível adicionar mais tarefas.\n");
         return;
     }
 
-    struct Task newTask;
     printf("Digite a prioridade da sua tarefa (0 a 10): ");
     scanf("%d", &newTask.priority);
 
@@ -18,6 +22,9 @@ void cadastrarTarefa(struct Task tasks[], int *numTasks) {
 
     printf("Digite a categoria da sua tarefa (até 100 caracteres): ");
     fgets(newTask.category, sizeof(newTask.category), stdin);
+
+    printf("Digite o estado da sua tarefa (completo/em andamento/não iniciado): ");
+    fgets(newTask.state, sizeof(newTask.state), stdin);
 
     tasks[*numTasks] = newTask;
     (*numTasks)++;
@@ -36,6 +43,7 @@ void listarTarefas(struct Task tasks[], int numTasks) {
         printf("Prioridade: %d\n", tasks[i].priority);
         printf("Descrição: %s", tasks[i].description);
         printf("Categoria: %s", tasks[i].category);
+        printf("Estado: %s", tasks[i].state);
         printf("--------------------\n");
     }
 }
@@ -52,4 +60,133 @@ void deletarTarefa(struct Task tasks[], int *numTasks, int index) {
 
     (*numTasks)--;
     printf("Tarefa deletada com sucesso! :)\n");
+}
+
+void alterarTarefa(struct Task tasks[], int numTasks) {
+    if (numTasks == 0) {
+        printf("Não há tarefas cadastradas.\n");
+        return;
+    }
+
+    int index;
+    printf("Digite o índice da tarefa a ser alterada: ");
+    scanf("%d", &index);
+
+    if (index < 1 || index > numTasks) {
+        printf("Índice inválido.\n");
+        return;
+    }
+
+    struct Task *task = &tasks[index - 1];
+
+    printf("Escolha o campo a ser alterado:\n");
+    printf("1. Prioridade\n");
+    printf("2. Descrição\n");
+    printf("3. Categoria\n");
+    printf("4. Estado\n");
+    printf("Escolha uma opção: ");
+
+    int opcao;
+    scanf("%d", &opcao);
+
+    switch (opcao) {
+        case 1:
+            printf("Digite a nova prioridade: ");
+            scanf("%d", &task->priority);
+            break;
+        case 2:
+            printf("Digite a nova descrição: ");
+            getchar(); // Consumir o caractere de nova linha residual
+            fgets(task->description, sizeof(task->description), stdin);
+            break;
+        case 3:
+            printf("Digite a nova categoria: ");
+            getchar();
+            fgets(task->category, sizeof(task->category), stdin);
+            break;
+        case 4:
+            printf("Digite o novo estado (completo/em andamento/não iniciado): ");
+            getchar();
+            fgets(task->state, sizeof(task->state), stdin);
+            break;
+        default:
+            printf("Opção inválida.\n");
+            return;
+    }
+
+    printf("Tarefa alterada com sucesso!\n");
+}
+
+void filtrarTarefasPorPrioridade(struct Task tasks[], int numTasks) {
+    if (numTasks == 0) {
+        printf("Não há tarefas cadastradas.\n");
+        return;
+    }
+
+    int prioridadeFiltro;
+    printf("Digite a prioridade desejada: ");
+    scanf("%d", &prioridadeFiltro);
+
+    int encontrouTarefa = 0;
+
+    printf("Tarefas com prioridade %d:\n", prioridadeFiltro);
+    for (int i = 0; i < numTasks; i++) {
+        if (tasks[i].priority == prioridadeFiltro) {
+            printf("Tarefa %d:\n", i + 1);
+            printf("Prioridade: %d\n", tasks[i].priority);
+            printf("Descrição: %s", tasks[i].description);
+            printf("Categoria: %s", tasks[i].category);
+            printf("Estado: %s", tasks[i].state);
+            printf("--------------------\n");
+            encontrouTarefa = 1;
+        }
+    }
+
+    if (!encontrouTarefa) {
+        printf("Nenhuma tarefa encontrada com prioridade %d.\n", prioridadeFiltro);
+    }
+}
+
+int main() {
+    int opcao;
+    do {
+        printf("==(¬‿¬)== Gerenciador de Tarefas ==(¬‿¬)==\n");
+        printf("1. Cadastrar Tarefa :)\n");
+        printf("2. Listar Tarefas\n");
+        printf("3. Deletar Tarefa :(\n");
+        printf("4. Alterar Tarefa\n");
+        printf("0. Sair ;(\n");
+        printf("Escolha uma opção: ");
+        
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                cadastrarTarefa(tasks, &numTasks);
+                break;
+            case 2:
+                listarTarefas(tasks, numTasks);
+                break;
+            case 3: {
+                int index;
+                printf("Digite o índice da tarefa a ser deletada: ");
+                scanf("%d", &index);
+                deletarTarefa(tasks, &numTasks, index - 1);
+                break;
+            }
+            case 4:
+                alterarTarefa(tasks, numTasks);
+                break;
+            case 0:
+                printf("Encerrando o programa.\n");
+                break;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+        }
+        
+        while (getchar() != '\n');
+        
+    } while (opcao != 0);
+
+    return 0;
 }
--- lab.h
+++ lab.h
@@ -5,10 +5,13 @@ struct Task {
     int priority;
     char description[300];
     char category[100];
+    char state[20];
 };
 
 void cadastrarTarefa(struct Task tasks[], int *numTasks);
 void listarTarefas(struct Task tasks[], int numTasks);
 void deletarTarefa(struct Task tasks[], int *numTasks, int index);
+void alterarTarefa(struct Task tasks[], int numTasks);
+void filtrarTarefasPorPrioridade(struct Task tasks[], int numTasks);
 
 #endif
